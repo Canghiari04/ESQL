36e11ed27403c3b68e3e83ab8ccef02ea58d05f1
Canghiari04 <matteo.canghiari@gmail.com>
2024-01-20 18:03:59 +0100

    .

---
+++ Prova1/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+echo "Ciao";
+?>
+
+</html>
---
+++ Prova1/login.php
@@ -0,0 +1,7 @@
+<!DOCTYPE hmtl PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<?php 
+
+echo "Hello world";
+$x = 0;
+?>
