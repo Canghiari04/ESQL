e06448ecca755d46c501c7f370b3f1d94cd5f3f5
Canghiari04 <matteo.canghiari@gmail.com>
2024-01-27 17:39:59 +0100

    fix di textbox e campo telefono

--- Project/php/authentication.php
+++ Project/php/authentication.php
@@ -7,7 +7,7 @@
             $email = $_POST["txtEmailLogin"];
             $password = $_POST["txtPasswordLogin"];
 
-            /* query definita per la ricerca dell'utente nelle tabelle Docente e Studente*/
+            /* mai mettere COUNT(*) nelle query di login, restituisce sempre un valore maggiore di 0 */
             $sql = "SELECT EMAIL FROM Utente WHERE (EMAIL=:labelEmail) AND (PSWD=:labelPassword)";
             
             try {
@@ -21,11 +21,10 @@
                     $tipo =  loginUtente($conn, $email);
                     
                     if($tipo == "Studente") {
-                        echo($tipo);
-                        /* reindirizzare la pagina rispetto alla sezione studente */                        
+                        /* metodo per reindirizzare tramite uso di HTTP */
+                        header("Location: handlerStudente.php");                      
                     } else {
-                        /* reindirizzare la pagina rispetto alla sezione docente */                        
-                        echo($tipo);
+                        header("Location: handlerStudente.php");
                     }
                 } else {
                     /* messageBox a video, data la presenza già dell'utente */
@@ -34,20 +33,17 @@
                 echo 'Eccezione individuata: '. $e -> getMessage();
             }   
 
+            /* chiusura necessaria, poichè se si indirizza in una nuova pagina php-html potrebbe riportare errori a livello di definition-manipulation del db */
             closeConnection($conn);
         } elseif (isset($_POST["txtEmailSignupStudente"])) {
             $email = $_POST["txtEmailSignupStudente"];
             $password = $_POST["txtPasswordSignupStudente"];
             $nome = $_POST["txtNomeSignupStudente"];
             $cognome = $_POST["txtCognomeSignupStudente"];
-
-            /* permettere l'inserimento null del numero di telefofono */
             $telefono = $_POST["txtTelefonoSignupStudente"];
-
             $annoImmatricolazione = $_POST["txtAnnoImmatricolazione"];
             $codice = $_POST["txtCodice"];
             
-            /* mai mettere COUNT(*) nelle query di login, restituisce sempre 1 */
             $sql = "SELECT EMAIL FROM Utente JOIN Studente ON (Utente.EMAIL=Studente.EMAIL_STUDENTE) WHERE (EMAIL=:labelEmail) AND (PSWD=:labelPassword)";
             
             try {
@@ -60,26 +56,25 @@
                 if($numRows > 0){
                     /* messageBox che evidenzia la presenza dell'utente, magari con un suggerimento dopo errore ripetuto */
                 } else{
+                    /* controllo per verifica assenza di recapito telefonico */
+                    $telefono = controlTelefono($telefono);
                     insertStudente($conn, $email, $password, $nome, $cognome, $telefono, $annoImmatricolazione, $codice);
+                    header("Location: login.php");
                 }
             } catch(Exception $e) {
                 echo 'Eccezione individuata: '. $e -> getMessage();
             }  
 
-            //closeConnection($conn);
+            closeConnection($conn);
         } elseif (isset($_POST["txtEmailSignupDocente"])) {
             $email = $_POST["txtEmailSignupDocente"];
             $password = $_POST["txtPasswordSignupDocente"];
             $nome = $_POST["txtNomeSignupDocente"];
             $cognome = $_POST["txtCognomeSignupDocente"];
-
-            /* permettere l'inserimento null del numero di telefofono */
             $telefono = $_POST["txtTelefonoSignupDocente"];
-
             $corso = $_POST["txtCorso"];
             $dipartimento = $_POST["txtDipartimento"];
 
-            /* mai mettere COUNT(*) nelle query di login, restituisce sempre 1 a livello di numero di righe*/
             $sql = "SELECT EMAIL FROM Utente JOIN Docente ON (Utente.EMAIL=Docente.EMAIL_DOCENTE) WHERE (EMAIL = :labelEmail) AND (PSWD = :labelPassword)";
             
             try {
@@ -92,21 +87,25 @@
                 if($numRows > 0) {
                     /* messageBox che evidenzia la presenza dell'utente, magari con un suggerimento dopo errore ripetuto */
                 } else {
+                    $telefono = controlTelefono($telefono);
                     insertDocente($conn, $email, $password, $nome, $cognome, $telefono, $dipartimento, $corso);
+                    header("Location: login.php");
                 }
             } catch(Exception $e) {
                 echo 'Eccezione individuata: '. $e -> getMessage();
             }  
 
-            //closeConnection($conn);
+            closeConnection($conn);
         } 
     }
 
     function loginUtente($conn, $email){
         $sql = "SELECT EMAIL FROM Utente JOIN Studente ON (EMAIL=EMAIL_STUDENTE) WHERE (EMAIL=:labelEmail)";
+        
         $result = $conn -> prepare($sql);
         $result -> bindValue(":labelEmail", $email);
         $result -> execute();
+        
         $numRows = $result -> rowCount();
         
         if($numRows > 0) {
@@ -114,14 +113,16 @@
         } else {
             return "Docente";
         }
-
-        /* effettuare controlli */
     }
 
     function insertStudente($conn, $email, $password, $nome, $cognome, $telefono, $annoImmatricolazione, $codice) {
+        /* string per richiamare la stored procedure, senza che sia posti i campi */
         $storedProcedure = "CALL Registrazione_Studente(:labelEmail, :labelPassword, :labelNome, :labelCognome, :labelTelefono, :labelAnno, :labelCodice)";
+        
+        /* si crea lo statement necessario per richiamare la stored procedure  */
         $stmt = $conn -> prepare($storedProcedure);
 
+        /* si associano i valori della stored procedure rispetto ai campi estrapolati */
         $stmt -> bindValue(":labelEmail", $email);
         $stmt -> bindValue(":labelPassword", $password);
         $stmt -> bindValue(":labelNome", $nome);
@@ -130,9 +131,8 @@
         $stmt -> bindValue(":labelAnno", $annoImmatricolazione);
         $stmt -> bindValue(":labelCodice", $codice);
 
+        /* si esegue la stored procedure */
         $stmt -> execute();
-
-        /* effettuare controlli */
     }
 
     function insertDocente($conn, $email, $password, $nome, $cognome, $telefono, $dipartimento, $corso) {
@@ -148,7 +148,12 @@
         $stmt -> bindValue(":labelCorso", $corso);
 
         $stmt -> execute();
+    }
 
-        /* effettuare controlli */
+    function controlTelefono($telefono) {
+        /* funzione necessaria, dato che i tag possono restituire solo valori di default di tipo stringa */
+        if($telefono == "NULL") {
+            return NULL;
+        }
     }
 ?>
