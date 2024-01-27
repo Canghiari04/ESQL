c166e560b31f53010aea9c775a4847c63f2e6dd0
Canghiari04 <matteo.canghiari@gmail.com>
2024-01-27 17:43:13 +0100

    indentazione del codice

--- Project/php/index.php
+++ Project/php/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Public Sans' rel='stylesheet'>
