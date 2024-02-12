395dd19fb392c96c4ca19fcc80e9d9ef41957ddc
Canghiari04 <matteo.canghiari@gmail.com>
2024-02-12 21:30:10 +0100

    .

--- Project/php/question.php
+++ Project/php/question.php
@@ -10,7 +10,7 @@
         </style>
     </head>
     <body>
-        <form action="insert.php" method="POST">
+        <form action="insert/insertQuestion.php" method="POST">
             <div class="navbar">
                 <a><img class="zoom-on-img" width="112" height="48" src="img/ESQL.png"></a>
                 <a><button class="navbar-button" type="submit" name="btnInsertDomandaChiusa">Add Chiusa</button></a>
