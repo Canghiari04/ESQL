fef4eaa3fadb559c8cbaf49bdcb0d0f30e2cae81
Canghiari04 <matteo.canghiari@gmail.com>
2024-02-16 22:33:32 +0100

    corretta indentazione e disposizione tag html

--- Project/php/question.php
+++ Project/php/question.php
@@ -10,73 +10,70 @@
         </style>
     </head>
     <body>
-        <form action="insert/insertQuestion.php" method="POST">
-            <div class="navbar">
-                <a><img class="zoom-on-img" width="112" height="48" src="img/ESQL.png"></a>
-                <div class="dropdown">
-                    <button class="dropbtn">Add Question</button>
-                    <div class="dropdown-content">
-                        <a href="insert/insertQuestion.php?chiusa">Chiusa</a>
-                        <a href="insert/insertQuestion.php?codice">Codice</a>
-                    </div>
+        <div class="navbar">
+            <a><img class="zoom-on-img" width="112" height="48" src="img/ESQL.png"></a>
+            <div class="dropdown">
+                <button class="dropbtn">Add Question</button>
+                <div class="dropdown-content">
+                    <a href="insert/insertQuestion.php?chiusa">Chiusa</a>
+                    <a href="insert/insertQuestion.php?codice">Codice</a>
                 </div>
-                <a href="handlerDocente.php"><img class="zoom-on-img undo" width="32" height="32" src="img/undo.png"></a>
             </div>
-        </form>
-        <div>
-            <?php 
-                include 'connectionDB.php';
-                $conn = openConnection();
-
-                $sql = "SELECT * FROM Quesito";
-                
-                try {
-                    $result = $conn -> prepare($sql);
-                    $result -> execute();
-                    $numRows = $result -> rowCount();
-
-                    if($numRows > 0) {
-                        echo '
-                            <div class="div-th"> 
-                                <table class="table-head">   
-                                    <tr>  
-                                        <th>Difficoltà</th>
-                                        <th>Descrizione</th>
-                                        <th>Numero risposte</th>
-                                    </tr>
-                                </table>
-                            </div>
-                        ';
-
-                        while($row = $result -> fetch(PDO::FETCH_OBJ)) {
-                            echo '  
-                                <div class="div-td">
-                                    <table class="table-list">
-                                        <tr>
-                                            <th>'.$row -> DIFFICOLTA.'</th>
-                                            <th>'.$row -> DESCRIZIONE.'</th>
-                                            <th>'.$row -> NUM_RISPOSTE.'</th>
-                                            <form action="specifics/specifics.php" method="GET">
-                                                <th><button class="table-button" type="submit" name="btnSpecificQuestion" value='.$row -> ID.'>Options</button></th>
-                                            </form>
-                                            <form action="delete/deleted.php" method="GET">
-                                                <th><button class="table-button" type="submit" name="btnDropQuestion" value='.$row -> ID.'>Drop Question</button></th>
-                                            </form>
-                                            <form action="" method="GET">
-                                                <th><button class="table-button" type="submit" name="btnAddOption">Add Option</button></th>
-                                            </form>
-                                        </tr>
-                                    </table>
-                                </div>
-                            ';
-                        }
-                    }
-
-                    closeConnection($conn);
-                } catch (PDOException $e) {
-                    echo 'Eccezione: '. $e -> getMessage();
-                }
-            ?>
+            <a href="handlerDocente.php"><img class="zoom-on-img undo" width="32" height="32" src="img/undo.png"></a>
         </div>
+        <?php 
+            include 'connectionDB.php';
+            $conn = openConnection();
+
+            $sql = "SELECT * FROM Quesito";
+                
+            try {
+                $result = $conn -> prepare($sql);
+
+                $result -> execute();
+                $numRows = $result -> rowCount();
+            } catch (PDOException $e) {
+                echo 'Eccezione: '. $e -> getMessage();
+            }
+
+            if($numRows > 0) {
+                    echo '
+                        <div class="div-th"> 
+                            <table class="table-head">   
+                                <tr>  
+                                    <th>Descrizione</th>
+                                    <th>Difficoltà</th>
+                                    <th>Numero risposte</th>
+                                </tr>
+                            </table>
+                        </div>
+                    ';
+
+                while($row = $result -> fetch(PDO::FETCH_OBJ)) {
+                    echo '  
+                        <div class="div-td">
+                            <table class="table-list">
+                                <tr>
+                                    <th>'.$row -> DESCRIZIONE.'</th>
+                                    <th>'.$row -> DIFFICOLTA.'</th>
+                                    <th>'.$row -> NUM_RISPOSTE.'</th>
+                                    <form action="specifics/specificQuestion.php" method="POST">
+                                        <th><button class="table-button" type="submit" name="btnSpecificQuestion" value='.$row -> ID.'>Options</button></th>
+                                    </form>
+                                    <form action="delete/deleted.php" method="GET">
+                                        <th><button class="table-button" type="submit" name="btnDropQuestion" value='.$row -> ID.'>Drop Question</button></th>
+                                    </form>
+                                    <form action="" method="GET">
+                                        <th><button class="table-button" type="submit" name="btnAddOption">Add Option</button></th>
+                                    </form>
+                                </tr>
+                            </table>
+                        </div>
+                    ';
+                }
+            }
+
+            closeConnection($conn);
+        ?>
     </body>
 </html>
