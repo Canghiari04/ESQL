3c44ebd5a48b380eee3b32b1c0ce3b91c4860fbb
Canghiari04 <matteo.canghiari@gmail.com>
2024-02-16 22:55:51 +0100

    corretta indentazione e tag html

---
+++ Project/php/specifics/specificQuestion.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Public Sans' rel='stylesheet'>
+        <style>
+            <?php
+                include '../connectionDB.php';
+                include '../css/specifics.css';
+            ?>
+        </style>
+    </head>
+    <body>
+        <div class="navbar">
+            <a><img class="zoom-on-img ESQL" width="112" height="48" src="../img/ESQL.png"></a>
+            <a href="../question.php"><img class="zoom-on-img undo" width="32" height="32" src="../img/undo.png"></a>
+        </div>
+        <?php 
+            $conn = openConnection();
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if(isset($_POST['btnSpecificQuestion'])) {
+                    $idQuestion = $_POST["btnSpecificQuestion"];
+
+                    $type = getTypeQuestion($conn, $idQuestion);
+
+                    if($type == 'CHIUSA') {
+                        $sql = 'SELECT TESTO FROM Opzione_Risposta WHERE (Opzione_Risposta.ID_DOMANDA_CHIUSA=:idQuesito)';
+                    } else {
+                        $sql = 'SELECT TESTO FROM Sketch_Codice  WHERE (Sketch_Codice.ID_DOMANDA_CODICE=:idQuesito)';
+                    }
+
+                    try {
+                        $result = $conn -> prepare($sql);
+                        $result -> bindValue(':idQuesito', $idQuestion);
+
+                        $result -> execute();
+                    } catch (PDOException $e) {
+                        echo 'Eccezione '.$e -> getMessage().'<br>';
+                    }
+                        
+                    if($result) {
+                        while($row = $result->fetch(PDO::FETCH_OBJ)) {
+                            echo '
+                                <div class="div-question">
+                                    <table>   
+                                        <tr>  
+                                            <th>Risposta</th>
+                                        </tr>
+                                        <tr>  
+                                            <td>'.$row -> TESTO.'</tc>
+                                        </tr>
+                                    </table>
+                                </div>
+                            ';
+                        }
+                    }   
+                }
+            }
+
+            function getTypeQuestion($conn, $idQuestion) {
+                $sql = 'SELECT * FROM Quesito JOIN Domanda_Chiusa ON (ID=ID_DOMANDA_CHIUSA) WHERE (Quesito.ID=:idQuesito);';
+
+                try {
+                    $result = $conn -> prepare($sql);
+                    $result -> bindValue(':idQuesito', $idQuestion);
+
+                    $result -> execute();
+                    $numRows = $result -> rowCount();
+                } catch(PDOException $e) {
+                    echo 'Eccezione '.$e -> getMessage().' <br>';
+                }
+
+                if($numRows > 0) {
+                    return 'CHIUSA';
+                } else {
+                    return 'CODICE';
+                }
+            }
+            
+            closeConnection($conn);
+        ?>
+    </body>
+</html>
---
+++ Project/php/specifics/specificTable.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Public Sans' rel='stylesheet'>
+        <style>
+            <?php
+                include '../connectionDB.php';
+                include '../css/specifics.css';
+            ?>
+        </style>
+    </head>
+    <body>
+        <div class="navbar">
+            <a><img class="zoom-on-img ESQL" width="112" height="48" src="../img/ESQL.png"></a>
+            <a href="../table_exercise.php"><img class="zoom-on-img undo" width="32" height="32" src="../img/undo.png"></a>
+        </div>
+        <div>
+            <?php 
+                $conn = openConnection();
+
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {   
+                    if(isset($_POST['btnSpecificTable'])) {
+                        $idTable = $_POST['btnSpecificTable'];    
+
+                        $sql = 'SELECT Attributo.TIPO, Attributo.NOME, Attributo.CHIAVE_PRIMARIA FROM Tabella_Esercizio JOIN Attributo ON (Tabella_Esercizio.ID=Attributo.ID_TABELLA) WHERE (Tabella_Esercizio.ID=:idTable)';
+                        
+                        try {
+                            $result = $conn -> prepare($sql);
+
+                            $result -> bindValue(':idTable', $idTable);
+                            $result -> execute();
+                        } catch (PDOException $e) {
+                            echo 'Eccezione: '.$e -> getMessage().'<br>'; 
+                        }
+                            
+                        echo '
+                            <div class="div-th"> 
+                                <table class="table-head">   
+                                    <tr>  
+                                        <th>Nome</th>
+                                        <th>Tipo</th>
+                                        <th>Chiave primaria</th>
+                                    </tr>
+                                </table>
+                            </div>
+                        ';
+                            
+                        if($result) {
+                            while($row = $result->fetch(PDO::FETCH_OBJ)) {
+                                /* metodo che restituisce se l'attributo visualizzato costituisca o meno la chiave primaria della tabella */
+                                $primaryKey = convertPrimaryKey($row -> CHIAVE_PRIMARIA);
+                                echo '
+                                    <div class="div-td">
+                                        <table class="table-list">   
+                                            <tr>  
+                                                <th>'.$row -> NOME.'</th>
+                                                <th>'.$row -> TIPO.'</th>
+                                                <th>'.$primaryKey.'</th>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                ';
+                            }
+                        }
+                    }
+                }
+
+                function convertPrimaryKey($value) {
+                    if($value == 0) {
+                        return 'No';
+                    } else {
+                        return 'Si';
+                    }
+                }
+                
+                closeConnection($conn);
+            ?>
+        </div>
+    </body>
+</html>
