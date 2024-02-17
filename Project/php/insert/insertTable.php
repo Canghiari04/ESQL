80adf02add9a5d4085ed73a805929efb79c5f038
Canghiari04 <matteo.canghiari@gmail.com>
2024-02-17 22:16:16 +0100

    importato comando web responsive

--- Project/php/insert/insertTable.php
+++ Project/php/insert/insertTable.php
@@ -4,6 +4,8 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Public Sans' rel='stylesheet'> 
         <style>
             <?php 
                 include 'addRecord.php';
