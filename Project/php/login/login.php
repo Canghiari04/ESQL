bb4d0e0af5dc196150335ed34ed69e7d5369d667
Canghiari04 <matteo.canghiari@gmail.com>
2024-02-18 16:43:58 +0100

    corretto tag include style css e singoli apici

--- Project/php/login/login.php
+++ Project/php/login/login.php
@@ -3,11 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href='https://fonts.googleapis.com/css?family=Public Sans' rel='stylesheet'>
-        <style>
-            <?php
-                include '../css/login.css';
-            ?>
-        </style>
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
     </head>
     <body>
         <div class="navbar">
