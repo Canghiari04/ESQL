af47f27adb2a9612dd87d6a4fe468f8841eb0074
ossamanadifi <ossamanadifi@gmail.com>
2024-03-12 14:45:21 +0100

    fix visualizzazione stato test; controllo aggiuntivo sulla creazione tabella; cancellazione corretta dei quesiti in afferenza

--- Project/php/connectionDB.php
+++ Project/php/connectionDB.php
@@ -1,43 +1,43 @@
-<?php    
+<?php
     function openConnection() {
         try {
-            $pdo = new PDO('mysql:host=localhost;dbname=ESQLDB', 'root', 'password');
+            $pdo = new PDO('mysql:host=localhost;dbname=ESQLDB', 'root', '');
             $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+
             return $pdo;
         }
         catch(PDOException $e) {
-            echo "Eccezione ".$e -> getMessage()."<br>";
+            echo 'Eccezione '.$e -> getMessage().'<br>';
             return null;
+        }
+    }
+
+    function openConnectionMongoDB() {
+        date_default_timezone_set('Europe/Rome');
+
+        $mongoHost = 'localhost';
+        $mongoPort = 27017;
+
+        //$manager = new MongoDB\Driver\Manager("mongodb://$mongoHost:$mongoPort");
+
+        //return $manager;
+    }
+
+    function writeLog($manager, $document) {
+        $mongoDatabase = 'ESQL';
+        $mongoCollection = 'logs';
+
+        //$bulkWrite = new MongoDB\Driver\BulkWrite;
+        //$bulkWrite->insert($document);
+
+        try{
+            //$manager->executeBulkWrite("$mongoDatabase.$mongoCollection", $bulkWrite);
+        } catch (Exception $e){
+            //echo($e -> getMessage());
         }
     }
 
     function closeConnection($pdo) {
         $pdo = null;
     }
-
-    function openConnectionMongoDB() {
-        date_default_timezone_set("Europe/Rome");
-
-        $mongoHost = "localhost";
-        $mongoPort = 27017;
-
-        $manager = new MongoDB\Driver\Manager("mongodb://$mongoHost:$mongoPort");
-
-        return $manager;
-    }
-
-    function writeLog($manager, $document) {
-        $mongoDatabase = "ESQL";
-        $mongoCollection = "logs";
-
-        $bulkWrite = new MongoDB\Driver\BulkWrite;
-        $bulkWrite->insert($document);
-
-        try{
-            $manager->executeBulkWrite("$mongoDatabase.$mongoCollection", $bulkWrite);
-        } catch (Exception $e){
-            //echo($e -> getMessage());
-        }
-    }
-?> 
+?>
