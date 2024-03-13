f25d4f946c4615bc030ad62a20207217d3d05525
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-13 01:12:41 +0100

    corretto reindirizzamento tra pagine

--- Project/php/docente/handlerDocente.php
+++ Project/php/docente/handlerDocente.php
@@ -17,7 +17,7 @@
         <div class="navbar">
             <a href="index.php"><img class="zoom-on-img ESQL" width="112" height="48" src="../style/img/ESQL.png"></a>
             <a class="a-href" href="../shared/statistic.php">Statistiche</a>
-            <a class="a-href" href="#">Messaggi</a>
+            <a class="a-href" href="../shared/message/message.php">Messaggi</a>
             <a class="a-href" href="table_exercise.php">Tabelle</a>
             <a class="a-href" href="test.php">Test</a>
             <a class="a-href" href="../index.php">Logout</a>
