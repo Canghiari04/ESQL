b5678699ecfd3377438618ef00b944941aaaf0e6
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-14 14:58:26 +0100

    corretto controllo degli accessi

--- Project/php/shared/handlerMessage/message.php
+++ Project/php/shared/handlerMessage/message.php
@@ -1,13 +1,9 @@
 <?php
     session_start();    
 
-    if(!isset($_SESSION["emailStudente"])) {
+    if ((!isset($_SESSION["emailStudente"])) AND (!isset($_SESSION["emailDocente"]))) {
         header("Location: ../login/login.php");
-        exit(); 
-    } elseif(!isset($_SESSION["emailDocente"])) {
-        header("Location: ../login/login.php");
-        exit();
-    }
+    } 
 ?>
 <!DOCTYPE html>
 <html>
