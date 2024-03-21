4ee4d42a3b83061d979ec57f79dc4a22af2371ed
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-21 10:38:39 +0100

    pagina che contiene i metodi più invocati

--- Project/php/shared/handlerMessage/buildFormMessage.php
+++ Project/php/shared/handlerMessage/buildFormMessage.php
@@ -1,4 +1,90 @@
 <?php
+    /* metodo che permette di visualizzare correttamente la navbar */
+    function buildNavbar($typeUser) {
+        if($typeUser == "Teacher") {
+            $nameFile = "../../docente/handlerDocente.php";
+        } else {
+            $nameFile = "../../studente/handlerStudente.php";
+        }
+
+        echo '
+            <div class="navbar">
+                <a><img class="zoom-on-img ESQL" width="112" height="48" src="../../style/img/ESQL.png"></a>
+                <form action="newMessage.php" method="POST">
+                    <button type="submit" class="button-navbar-first" name="btnNewMessage" value="'.$typeUser.'">New Message</button>
+                </form>
+                <form action="viewMessagesReceived.php" method="POST">
+                    <button type="submit" class="button-navbar-second" name="btnViewMessages" value="'.$typeUser.'">Received Messages</button>
+                </form>
+                <a href="'.$nameFile.'"><img class="zoom-on-img undo" width="32" height="32" src="../../style/img/undo.png"></a>
+            </div>
+        ';
+    }
+
+
+    /* funzione che permette di visualizzare tutti i messaggi inviati dallo specifico utente */
+    function buildMessageTest($conn, $typeUser) {
+        /* in base alla tipologia e alla mail è diversificata l'interrogazione posta al database */
+        if($typeUser == "Teacher") {
+            $sql = "SELECT * FROM Messaggio WHERE (ID NOT IN (SELECT Messaggio_Studente.ID_MESSAGGIO_STUDENTE FROM Messaggio_Studente)) AND (Messaggio.EMAIL_DOCENTE=:emailDocente);";
+            
+            try{
+                $result = $conn -> prepare($sql);
+                $result -> bindValue(":emailDocente", $_SESSION["emailDocente"]);
+                
+                $result -> execute();
+            } catch(PDOException $e) {
+                echo "Eccezione ".$e -> getMessage()."<br>";
+            }
+        } else {
+            $sql = "SELECT * FROM Messaggio, Messaggio_Studente WHERE (Messaggio.ID=Messaggio_Studente.ID_MESSAGGIO_STUDENTE) AND (Messaggio_Studente.EMAIL_STUDENTE=:emailStudente);";
+            
+            try{
+                $result = $conn -> prepare($sql);
+                $result -> bindValue(":emailStudente", $_SESSION["emailStudente"]);
+                
+                $result -> execute();
+            } catch(PDOException $e) {
+                echo "Eccezione ".$e -> getMessage()."<br>";
+            }
+        }
+            
+        echo '
+            <div class="center">
+                <div class="div-th"> 
+                    <table class="table-head-message">   
+                        <tr>  
+                            <th>TITOLO</th>
+                            <th>TEST RIFERIMENTO</th>
+                            <th>DATA</th>
+                        </tr>
+                    </table>
+                </div>
+        ';
+            
+        if(isset($result)) {
+            while($row = $result->fetch(PDO::FETCH_OBJ)) {
+                echo '
+                        <div class="div-td">
+                            <table class="table-list">   
+                                <tr>  
+                                    <th>'.$row -> TITOLO.'</th>
+                                    <th>'.$row -> TITOLO_TEST.'</th>
+                                    <th>'.$row -> DATA_INSERIMENTO.'</th>
+                                    <th>
+                                        <form action="viewMessageSent.php" method="POST">
+                                            <button class="table-button" type="submit" name="btnViewMessage" value= "'.$row -> ID.'|?|'.$typeUser.'">View Message</button>
+                                        </form>
+                                    </th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                ';
+            }
+        }     
+    }              
+
     /* definizione del bottone undo all'interno della navbar */
     function buildButtonUndo($typeUser) {
         echo '
@@ -15,7 +101,7 @@
                 <div class="div-message">
                     <div class="div-content">
                         <div class="div-name">
-                            <label class="label-name">'.getNameSurname($conn, $email).'</label>
+                            <label class="label-name">'.$email.'</label>
                         </div>
                         <p>Oggetto del messaggio <span>'.$title.'</span></p>
                         <p>Messaggio di <span>'.getNameSurname($conn, $email).'</span>, relativo al test <span>'.$titleTest.'</span>.</p>
