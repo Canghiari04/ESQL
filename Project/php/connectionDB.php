3e746360b028cd06c4a99ddfc269ddc2bfcf6d2b
Canghiari04 <matteo.canghiari@gmail.com>
2024-03-25 17:29:41 +0100

    corretta disposizione codice

--- Project/php/connectionDB.php
+++ Project/php/connectionDB.php
@@ -1,21 +1,25 @@
 <?php
     function openConnection() {
         try {
-            $pdo = new PDO('mysql:host=localhost;dbname=ESQLDB', 'root', '');
+            $pdo = new PDO("mysql:host=localhost;dbname=ESQLDB", "root", "");
             $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $pdo;
         }
         catch(PDOException $e) {
-            echo 'Eccezione '.$e -> getMessage().'<br>';
+            echo "Eccezione ".$e -> getMessage()."<br>";
             return null;
         }
     }
 
-    function openConnectionMongoDB() {
-        date_default_timezone_set('Europe/Rome');
+    function closeConnection($pdo) {
+        $pdo = null;
+    }
 
-        $mongoHost = 'localhost';
+    function openConnectionMongoDB() {
+        date_default_timezone_set("Europe/Rome");
+
+        $mongoHost = "localhost";
         $mongoPort = 27017;
 
         $manager = new MongoDB\Driver\Manager("mongodb://$mongoHost:$mongoPort");
@@ -24,20 +28,16 @@
     }
 
     function writeLog($manager, $document) {
-        $mongoDatabase = 'ESQL';
-        $mongoCollection = 'logs';
+        $mongoDatabase = "ESQL";
+        $mongoCollection = "logs";
 
         $bulkWrite = new MongoDB\Driver\BulkWrite;
-        $bulkWrite->insert($document);
+        $bulkWrite -> insert($document);
 
         try{
-            $manager->executeBulkWrite("$mongoDatabase.$mongoCollection", $bulkWrite);
+            $manager -> executeBulkWrite("$mongoDatabase.$mongoCollection", $bulkWrite);
         } catch (Exception $e){
-            echo($e -> getMessage());
+            echo "Eccezione ".$e -> getMessage()."<br>";
         }
-    }
-
-    function closeConnection($pdo) {
-        $pdo = null;
     }
 ?>
